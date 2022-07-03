cc210b56e8a77dee15281fe7834933c884530617
Prabhu Tripathi <prabhupratpt@gmail.com>
2022-07-03 15:19:08 +0530

    final commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -23,29 +23,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-
-//
-// class HomePage extends StatefulWidget {
-//   late final String name;
-//   HomePage({required this.name});
-//
-//   @override
-//   State<HomePage> createState() => _HomePageState();
-// }
-//
-// class _HomePageState extends State<HomePage> {
-//
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         body: Column(
-//           children: [
-//             Text('$')
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
